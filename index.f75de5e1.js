74e28e144b6d4ad648aee0cb0ec845da2cfd5cab
Artem Shuhailo <artem0462@gmail.com>
2024-01-17 11:15:11 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const tableRows = document.querySelector("table").rows;
+[
+    ...tableRows
+].forEach((row)=>{
+    const cellCopy = row.cells[1].cloneNode(true);
+    const cellLast = row.cells[row.cells.length - 1];
+    cellLast.before(cellCopy);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
